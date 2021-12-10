dfad6f5ced5b09be8a39761ea328109447ab8c31
wakey001 <stefanwakeman001@hotmail.co.uk>
2021-12-10 22:53:48 +0000

    add all functions needed

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 from random import randint
 
-ships_sunk = {"computer": 0, "player": 0}
+scores = {"computer": 0, "player": 0}
 
 
 class GameBoard:
     """
-    Main board class.
-    """   
+    Main board class. Sets board size, the number of ships.
+    The player's name and the board type (player or computer)
+    Has methods for adding ships, and guesses, and printing board. 
+    """ 
     def __init__(self, size, num_of_ships, name, type):
         self.size = size
         self.num_of_ships = num_of_ships
@@ -45,3 +47,49 @@ def random_point(size):
     """
     return randint(0, size-1)
 
+
+def valid_coordinates(x, y, board):
+    pass
+
+
+def polpulate_board(board):
+    pass
+
+
+def make_guess(board):
+    pass
+
+
+def play_game(computer_board, player_board):
+    pass
+
+
+def new_game():
+    """
+    starts a new game. Sets the board size and number of ships, resets the
+    scores and initialises the boards.
+    """
+
+    size = 5
+    num_of_ships = 4
+    scores["computer"] = 0
+    scores["player"] = 0
+    print("-" * 35)
+    print(" Welcome to VS BATTLESHIP!")
+    print(f" Board Size: {size}. numof ships: {num_ships}")
+    print(" Top left corner is row: 0, col: 0")
+    print("-" * 35)
+    player_name = input("Please enter your name \n")
+    print("-" * 35)
+
+    computer_board = Board(size, num_of_ships, "Computer", type="computer")
+    player_board = Board(size, num_of_ships, player_name, type="player")
+
+    for _ in range(num_ships):
+        polpulate_board(player_board)
+        polpulate_board(computer_board)
+    
+    play_game(computer_board, player_board)
+
+
+new_game()
